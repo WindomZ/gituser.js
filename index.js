fdfc73ffd6e3bf71511b786e3dbe336f1c3c9717
WindomZ <git.windomz@gmail.com>
2017-05-03 08:40:22 +0800

    add index.js

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
  * Created by WindomZ on 17-4-22.
  */
 'use strict'
+
+const init = require('./lib/init')
+const add = require('./lib/add')
+const remove = require('./lib/remove')
+const list = require('./lib/list')
+const set = require('./lib/set')
+const unset = require('./lib/unset')
+
+module.exports = init
+
+module.exports.add = add
+module.exports.remove = remove
+module.exports.list = list
+module.exports.add = set
+module.exports.unset = unset
